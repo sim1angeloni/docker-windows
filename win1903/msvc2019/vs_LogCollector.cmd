9b8ac3c2786155af30f2d19a646d3b14e6e0ad61
Simone Angeloni <sineang01@gmail.com>
2020-05-24 12:46:55 +0100

    Using .net framework images + msvc2019

---
+++ win1903/msvc2019/vs_LogCollector.cmd
@@ -0,0 +1,14 @@
+@if not defined _echo echo off
+setlocal enabledelayedexpansion
+
+call %*
+if "%ERRORLEVEL%"=="3010" (
+    exit /b 0
+) else (
+    if not "%ERRORLEVEL%"=="0" (
+        set ERR=%ERRORLEVEL%
+        call C:\vs_Collect.exe -zip:C:\vs_Logs.zip
+
+        exit /b !ERR!
+    )
+)
